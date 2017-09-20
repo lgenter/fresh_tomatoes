d4fdf97c2f6f498479c589e6d8895d7fcb7310e7
lgenter <30508714+lgenter@users.noreply.github.com>
2017-09-20 15:51:49 -0400

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,8 @@
 import webbrowser
 
 #make a class that creates space for inputting movie information
+
+
 class Movie():
     #create movie information parameters inside class's initialized space
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
@@ -8,6 +10,8 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     #create object that uses the open() function to play the movie trailer
     def show_trailer(self):
+        
         webbrowser.open(self.trailer_youtube_url)
